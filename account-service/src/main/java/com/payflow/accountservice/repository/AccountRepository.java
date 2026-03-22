beeaf73ada239373b992b579d1c7ad5c1686d985
Ahmet Arif Akıncı <ahmetarifaknci@gmail.com>
2026-03-22 13:51:19 +0300

    feat: add debit/credit operations to account-service

--- account-service/src/main/java/com/payflow/accountservice/repository/AccountRepository.java
+++ account-service/src/main/java/com/payflow/accountservice/repository/AccountRepository.java
@@ -4,11 +4,14 @@ import com.payflow.accountservice.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     List<Account> findAccountsByUserId(Long id);
 
     boolean existsByAccountNumber(String accountNumber);
+
+    Optional<Account> findByAccountNumber(String accountNumber);
 
 }
